8480d9104d781684b0ff0a36cdfb574163bd8e14
alaydesai094 <alaydesai094@gmail.com>
2018-11-05 15:33:12 -0500

    login and signup page

---
+++ TinderApp/platforms/browser/www/js/index.js
@@ -0,0 +1,9 @@
+document.getElementById("signup-btn").addEventListener("click", signup);
+
+function signup() {
+  // debug:
+  console.log("signup button pressed!");
+  alert("signup button pressed!");
+	window.location.href = "signup.html";
+	
+}
