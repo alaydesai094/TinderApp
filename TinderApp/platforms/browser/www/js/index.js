a3f8bbb1ecf33f6c4ce392e90595e5d85d0d990f
alaydesai094 <alaydesai094@gmail.com>
2018-11-06 12:52:31 -0500

    SignUP With Database

--- TinderApp/platforms/browser/www/js/index.js
+++ TinderApp/platforms/browser/www/js/index.js
@@ -1,3 +1,77 @@
+document.addEventListener("deviceready", connectToDatabase);
+
+
+var db = null; 
+db = window.openDatabase("tinder", "1.0", "Tinder App", 2 * 1024 * 1024);
+
+function connectToDatabase() {
+  console.log("device is ready - connecting to database");
+  // 2. open the database. The code is depends on your platform!
+  if (window.cordova.platformId === 'browser') {
+    console.log("browser detected...");
+    // For browsers, use this syntax:
+      //(nameOfDb, version number, description, db size)
+    // By default, set version to 1.0, and size to 2MB
+  // db = window.openDatabase("Tinder", "1.0", "Tinder App",2 * 1024 * 1024);
+  }
+  else {
+    alert("mobile device detected");
+    console.log("mobile device detected!");
+    var databaseDetails = {"name":"Tinder.db", "location":"default"}
+    db = window.sqlitePlugin.openDatabase(databaseDetails);
+    console.log("done opening db");
+	alert("done opening db");
+
+	
+  }
+
+  if (!db) {
+    alert("databse not opened!");
+    return false;
+  }
+
+}
+
+
+// ERROR MESSAGES
+function onReadyTransaction( ){
+		console.log( 'Transaction completed' );
+		alert("Transaction completed");
+	}
+	function onSuccessExecuteSql( tx, results ){
+		console.log( 'Execute SQL completed' );
+	alert( "Execute SQL completed" );
+	}
+	function onError( err ){
+		console.log( err )
+	}
+
+// CREATE DB
+db.transaction(
+		function(query){
+		query.executeSql(
+				"CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT, name TEXT, Dob DATE, Location TEXT )",
+				[],
+				onSuccessExecuteSql,
+				onError
+			)
+		},
+		onError,
+		onReadyTransaction
+	)
+
+	db.transaction(
+		function(query){
+			var sql = "INSERT INTO users (email,password,name,Dob,Location) VALUES(?,?,?,?,?)";
+			query.executeSql( sql,['alaydesai094@gmail.com','1234','Alay','09-05-1994','Toronto'],
+			onSuccessExecuteSql,
+			onError )
+		},
+		onError,
+		onReadyTransaction
+	)
+	
+	
 document.getElementById("signup-btn").addEventListener("click", signup);
 
 function signup() {
@@ -7,3 +81,4 @@ function signup() {
 	window.location.href = "signup.html";
 	
 }
+
